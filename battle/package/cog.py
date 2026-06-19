b2e957144422eb317ba28d0aecaab2947efcc38d
HiboMan <hiboman@proton.me>
2026-06-19 16:30:38 +0200

    Add Black Hole, Empyrean, Elemental Buffs

--- battle/package/cog.py
+++ battle/package/cog.py
@@ -27,8 +27,11 @@ log = logging.getLogger("ballsdex.packages.battle")
 
 # Configuration constants
 SPECIAL_BUFFS = {
-    "✨": (2000, 2000), # Shiny buffs
-    "🔮": (5000, 5000), # Mythical buffs
+    "✨": (2000,2000), # Shiny Buffs
+    "🔮": (5000,5000), # Mythical Buffs
+    "🕳️🌌": (500,500), # Black Hole Buffs
+    "👁️🪽": (7500,7500), # Empyrean Buffs
+    "☄️": (1000,1000), # Elemental Buffs
 } # Special buffs
 # HP, ATK
 MAXSTATS = [15000, 15000] # Max stats a card is limited to (before buffs)
